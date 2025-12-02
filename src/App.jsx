48e12a73032f2f31e4db5fb2d52ebcef065b0c2a
Keyur Halpati <keyur.halpati@srkay.com>
2025-12-02 11:17:29 +0530

    change_page

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Mr Hacker</h1>
+      <h1>Mr Keyur</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
