e4eca07a3c4507060061f95d5458f9550a2e518b
Keyur Halpati <keyur.halpati@srkay.com>
2025-12-02 15:52:28 +0530

    networking done

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])
+
+  async function fetchUsers() {
+    const response = await axios.get('http://backend-app:3000/api/users')
+
+    setUsers(response.data.data)
+  }
+
+  fetchUsers()
 
   return (
     <>
@@ -18,12 +27,16 @@ function App() {
       </div>
       <h1>Hey ankit😎</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="user-list">
+          {users.map(user => (
+            <div key={user.id} className="user-card">
+              <h2>{user.name}</h2>
+              <p>Email: {user.email}</p>
+              <p>Age: {user.age}</p>
+              <p>City: {user.city}</p>
+            </div>
+          ))}
+        </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
