0f02252ebe4529182691a49851ab1e062cfa5285
Keyur Halpati <keyur.halpati@srkay.com>
2025-12-03 10:36:39 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hey ankit😎</h1>
+      <h1>Hey pyaj😎</h1>
       <div className="card">
         <div className="user-list">
           {users.map(user => (
